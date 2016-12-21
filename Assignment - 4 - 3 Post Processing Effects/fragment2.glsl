19a4df6c98611ae0d40b5d6ef225f16af9cd01f6
Srinivas P G <srinivas11789@gmail.com>
2016-12-21 00:56:58 -0500

    Update fragment2.glsl

--- Assignment - 4 - 3 Post Processing Effects/fragment2.glsl
+++ Assignment - 4 - 3 Post Processing Effects/fragment2.glsl
@@ -4,7 +4,6 @@ void main()
 {
 vec4 texColor = texture2D( screenFramebuffer2, texture2);
 float brightness = (texColor.x+texColor.y+texColor.z)/3.0;
-//gl_FragColor = vec4(1.0-(brightness * texture2D( screenFramebuffer2, texture2)).xyz, 1.0);
-gl_FragColor = vec4(brightness*(1.0-texture2D( screenFramebuffer2, texture2)).xyz, 1.0);
-//gl_FragColor = vec4(1.0,0.0,0.0,1.0);
+vec4 bw = vec4(brightness,brightness,brightness,1.0);
+gl_FragColor = vec4(1.0-texture2D( screenFramebuffer2, texture2).xyz, 1.0) + bw;
 }
